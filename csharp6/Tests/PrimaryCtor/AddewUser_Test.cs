ebe7dc09ac7b1d68ca968796860bf33e1f6523f6
K. Scott Allen <scott@OdeToCode.com>
2014-08-01 17:17:32 +0000

    primary constructor expriemnts

---
+++ csharp6/Tests/PrimaryCtor/AddewUser_Test.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LanguageFeatures;
+using LanguageFeatures.PrimaryCtor;
+
+namespace Tests.PrimaryCtor
+{
+    [TestClass]
+    public class AddewUser_Test
+    {
+        [TestMethod]
+        public void Can_Set_User_And_Creator()
+        {
+            var newUser = new User();
+            var creator = new User();
+            var command = new AddUserCommand(newUser, creator);
+
+            Assert.IsTrue(Object.ReferenceEquals(command.NewUser, newUser));
+            Assert.IsTrue(Object.ReferenceEquals(command.Creator, creator));
+            Assert.AreEqual(command.Creator.Id, command.CreatorId);
+
+        }
+
+        [TestMethod]
+        public void Will_Throw_If_No_Creator()
+        {
+            var threw = false;
+            var newUser = new User();
+
+            try
+            {
+                var command = new AddUserCommand(newUser, null);
+            }
+            catch (ArgumentNullException)
+            {
+                threw = true;
+            }
+
+            Assert.IsTrue(threw);
+        }
+
+        [TestMethod]
+        public void Order_Of_Appearance_Is_Signifigant()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Can_Use_Primary_Ctor_Parameters_In_Methods()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void Can_Have_Other_Ctors()
+        {
+            Assert.Fail();
+        }
+    }
+}
