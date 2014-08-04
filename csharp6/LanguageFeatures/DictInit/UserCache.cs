962406e8bf4761f3d3a903483552e3d2e1790eb0
K. Scott Allen <scott@OdeToCode.com>
2014-08-04 17:05:55 +0000

    placeholder for dictionary initializer, if it turns on

---
+++ csharp6/LanguageFeatures/DictInit/UserCache.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.DictInit
+{
+    public class UserCache
+    {
+        public User Add(User user)
+        {
+            return null;
+        }
+
+        public User Find(string name)
+        {
+            return null;
+        }
+
+        //Dictionary<string, User> _default =
+        //    new Dictionary<string, User>
+        //    {
+        //        ["admin"] = new User(),
+        //        ["guest"] = new User()
+        //    };
+    }    
+}
