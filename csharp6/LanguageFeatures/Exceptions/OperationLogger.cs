b987ffa678381ee25e58def2207077a5e43ec681
K. Scott Allen <scott@OdeToCode.com>
2014-08-09 14:24:06 +0000

    exceptions and conditional access

---
+++ csharp6/LanguageFeatures/Exceptions/OperationLogger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Exceptions
+{
+    public class OperationLogger : IDisposable
+    {
+        public OperationLogger(Stream logStream)
+        {
+            if (logStream == null)
+            {
+                throw new ArgumentNullException("logStream");
+            }
+            _logWriter = new StreamWriter(logStream);
+        }
+
+        public async Task LogOperation(Action operation)
+        {
+            var name = operation?.Method?.Name ?? "no name";
+
+            try
+            {
+                operation();
+                await _logWriter.WriteAsync(name + " executed");
+            }
+            catch(Exception ex) if(ex.Message != null)
+            {
+                await _logWriter.WriteAsync(name + " failed");
+            }
+            finally
+            {
+                await _logWriter.FlushAsync();
+            }            
+        }
+
+        readonly StreamWriter _logWriter;
+        private bool disposedValue = false; 
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _logWriter.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
---
+++ csharp6/Tests/Exceptions/OperationLogger_Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using LanguageFeatures.Exceptions;
+using System.Threading.Tasks;
+
+namespace Tests.Exceptions
+{
+    [TestClass]
+    public class OperationLogger_Tests
+    {
+        [TestMethod]
+        public async Task Can_Log_A_Successful_Operations()
+        {
+            var stream = new MemoryStream();
+            var logger = new OperationLogger(stream);
+
+            await logger.LogOperation(DoWork);
+
+            Assert.AreEqual("DoWork executed", await ReadStream(stream));
+
+        }
+
+        [TestMethod]
+        public async Task Can_Log_A_Failed_Operation()
+        {
+            var stream = new MemoryStream();
+            var logger = new OperationLogger(stream);
+
+            await logger.LogOperation(DoException);
+
+            Assert.AreEqual("DoException failed", await ReadStream(stream));
+
+        }
+
+        async Task<string> ReadStream(Stream stream)
+        {
+            var reader = new StreamReader(stream);
+            stream.Position = 0;
+            
+            var result = await reader.ReadToEndAsync();
+            return result.Trim();
+            
+        }
+
+        void DoWork()
+        {
+            // ...
+        }
+
+        void DoException()
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
