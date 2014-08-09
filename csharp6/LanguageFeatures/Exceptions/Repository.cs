5119f78d6c912b37e5035b7a826ee1916b393fd1
K. Scott Allen <scott@OdeToCode.com>
2014-08-09 03:10:24 +0000

    really bad exception code, just to see if example works

---
+++ csharp6/LanguageFeatures/Exceptions/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Exceptions
+{
+    public class Repository
+    {
+        public async Task<string> Read(string path)
+        {
+                try
+                {
+                using (var stream = File.Open(path, FileMode.Open))
+                using (var reader = new StreamReader(stream))
+                {
+                    var content = await reader.ReadToEndAsync();
+                    return content;
+                }
+            }
+            catch (FileNotFoundException ex)
+                if(ex.FileName == "foo.txt")
+            {
+                var x = 1;
+                var name = nameof(x);
+                using (var stream = File.Open(path, FileMode.Open))
+                using (var reader = new StreamReader(stream))
+                {
+                    await reader.ReadToEndAsync();
+                }
+            }
+            return "";
+        }
+
+    }
+}
